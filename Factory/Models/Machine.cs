0fa666300708489d4212d73421b5e2333399a26c
Kim Robinson <kimmykokonut@gmail.com>
2024-02-09 11:31:52 -0800

    add details action and view for machine

--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -8,8 +8,8 @@ public class Machine
   public int MachineId { get; set; }
   public string Name { get; set; }
   public string Description { get; set; }
-  public DateTime InstallDate { get; set; } = DateTime.now;
-  public string Inspection { get; set; }
+  public DateTime InstallDate { get; set; } = DateTime.Now;
+  public string Inspection { get; set; } = "";
   public string Status { get; set; }
   public List<EngineerMachine> JoinEntities { get; set; }
 
