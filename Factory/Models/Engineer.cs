046c0a1c0d7c477d1e9e2e8c611247abd171afcf
Kim Robinson <kimmykokonut@gmail.com>
2024-02-09 14:45:13 -0800

    add validation attributes for create engineer

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -1,16 +1,22 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Factory.Models; 
 
 public class Engineer
 {
   public int EngineerId { get; set; }
+  [Required(ErrorMessage = "You must enter a name")]
   public string Name { get; set; }
+  [Required(ErrorMessage = "You must enter a level")]
   public string Level { get; set; }
+  [Required(ErrorMessage = "You must enter a phone number containing 10 digits")]
+  [RegularExpression("^\d{10}$", ErrorMessage = "Number must be 10 digits and contain only numbers.")]
   public string Phone { get; set; }
   public DateTime HireDate { get; set; } = DateTime.Now;
   public bool IsActive { get; set; } = false;
+  [Required(ErrorMessage = "You must enter your license expiration date")]
   public string Renewal { get; set; }
   public List<EngineerMachine> JoinEntities { get; }
 }
