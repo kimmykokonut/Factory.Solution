e6b6420b79715266b9d233beeb9024f55a7861a2
Kim Robinson <kimmykokonut@gmail.com>
2024-02-09 12:11:28 -0800

    add read functionality for joinentities

--- Factory/Controllers/EngineersController.cs
+++ Factory/Controllers/EngineersController.cs
@@ -34,6 +34,8 @@ public class EngineersController : Controller
   public ActionResult Details(int id)
   {
     Engineer thisEngineer = _db.Engineers
+    .Include(engineer => engineer.JoinEntities)
+    .ThenInclude(join => join.Machine)
     .FirstOrDefault(engineer => engineer.EngineerId == id);
     return View(thisEngineer);
   }
--- Factory/Controllers/MachinesController.cs
+++ Factory/Controllers/MachinesController.cs
@@ -35,6 +35,8 @@ public class MachinesController : Controller
   public ActionResult Details(int id)
   {
     Machine thisMachine = _db.Machines
+    .Include(machine => machine.JoinEntities)
+    .ThenInclude(join => join.Engineer)
     .FirstOrDefault(machine => machine.MachineId == id);
     return View(thisMachine);
   }
